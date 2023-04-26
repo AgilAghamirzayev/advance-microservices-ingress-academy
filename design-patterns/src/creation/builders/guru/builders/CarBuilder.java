3ac08b4e260e705123fc44f905b3f8dda4e67449
Aqil Aghamirzayev <aqilzeka99@gmail.com>
2023-04-26 16:07:40 +0400

    Add builder design pattern

---
+++ design-patterns/src/creation/builders/guru/builders/CarBuilder.java
@@ -0,0 +1,51 @@
+package creation.builders.guru.builders;
+
+import creation.builders.guru.car.Car;
+import creation.builders.guru.car.CarType;
+import creation.builders.guru.components.Engine;
+import creation.builders.guru.components.GPSNavigator;
+import creation.builders.guru.components.Transmission;
+import creation.builders.guru.components.TripComputer;
+
+public class CarBuilder implements Builder {
+
+  private CarType type;
+  private int seats;
+  private Engine engine;
+  private Transmission transmission;
+  private TripComputer tripComputer;
+  private GPSNavigator gpsNavigator;
+
+  public void setCarType(CarType type) {
+    this.type = type;
+  }
+
+  @Override
+  public void setSeats(int seats) {
+    this.seats = seats;
+  }
+
+  @Override
+  public void setEngine(Engine engine) {
+    this.engine = engine;
+  }
+
+  @Override
+  public void setTransmission(Transmission transmission) {
+    this.transmission = transmission;
+  }
+
+  @Override
+  public void setTripComputer(TripComputer tripComputer) {
+    this.tripComputer = tripComputer;
+  }
+
+  @Override
+  public void setGPSNavigator(GPSNavigator gpsNavigator) {
+    this.gpsNavigator = gpsNavigator;
+  }
+
+  public Car getResult() {
+    return new Car(type, seats, engine, transmission, tripComputer, gpsNavigator);
+  }
+}
