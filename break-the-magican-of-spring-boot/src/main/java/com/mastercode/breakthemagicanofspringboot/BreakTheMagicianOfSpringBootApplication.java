33730da6f0cbad4efcf922c1ed2982b51adc8b6f
Aqil Aghamirzayev <aqilzeka99@gmail.com>
2023-05-13 10:09:10 +0400

    Enable Webmvc

--- break-the-magican-of-spring-boot/src/main/java/com/mastercode/breakthemagicanofspringboot/BreakTheMagicianOfSpringBootApplication.java
+++ break-the-magican-of-spring-boot/src/main/java/com/mastercode/breakthemagicanofspringboot/BreakTheMagicianOfSpringBootApplication.java
@@ -7,9 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @Configuration
+@EnableWebMvc
 public class BreakTheMagicianOfSpringBootApplication {
 
   @Bean
