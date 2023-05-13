6b396e08ddfde1b2aad819102cc7d2926f803ad1
Aqil Aghamirzayev <aqilzeka99@gmail.com>
2023-05-13 10:04:54 +0400

    Add TomcatServletWebServerFactory

--- break-the-magican-of-spring-boot/src/main/java/com/mastercode/breakthemagicanofspringboot/BreakTheMagicianOfSpringBootApplication.java
+++ break-the-magican-of-spring-boot/src/main/java/com/mastercode/breakthemagicanofspringboot/BreakTheMagicianOfSpringBootApplication.java
@@ -1,12 +1,21 @@
 package com.mastercode.breakthemagicanofspringboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class BreakTheMagicianOfSpringBootApplication {
 
+  @Bean
+  public TomcatServletWebServerFactory containerFactory() {
+    return new TomcatServletWebServerFactory();
+  }
+
   public static void main(String[] args) {
+
     SpringApplication.run(BreakTheMagicianOfSpringBootApplication.class, args);
   }
 
