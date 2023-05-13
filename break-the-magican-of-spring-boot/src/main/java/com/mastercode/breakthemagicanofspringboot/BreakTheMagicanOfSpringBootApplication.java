e335d995b3ea7cf5e96be88086ea75eccfd8619f
Aqil Aghamirzayev <aqilzeka99@gmail.com>
2023-05-13 09:52:49 +0400

    Initial commit

---
+++ break-the-magican-of-spring-boot/src/main/java/com/mastercode/breakthemagicanofspringboot/BreakTheMagicanOfSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.mastercode.breakthemagicanofspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BreakTheMagicanOfSpringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BreakTheMagicanOfSpringBootApplication.class, args);
+  }
+
+}
