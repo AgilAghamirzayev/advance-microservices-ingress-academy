61d7e91fe54bca1566d8c1b9026bb37f0ab08926
Aqil Aghamirzayev <aqilzeka99@gmail.com>
2023-05-20 03:21:12 +0400

    Add lombok and logging practice

---
+++ lombok-and-logging/src/test/java/az/ingress/lombokandlogging/LombokAndLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ingress.lombokandlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombokAndLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
